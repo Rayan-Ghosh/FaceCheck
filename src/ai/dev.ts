f220c8e8a35384dd101f2fe1bd756a033bf5d124
Rayan Ghosh <74374932+Rayan-Ghosh@users.noreply.github.com>
2025-09-07 22:40:50 +0530

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/teacher-face-verification.ts';
-import '@/ai/flows/student-face-verification.ts';
-import '@/ai/flows/register-user.ts';
